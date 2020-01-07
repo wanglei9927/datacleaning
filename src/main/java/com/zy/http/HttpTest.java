3e95d2e3dfc42f5bb0dbf83b75e2e3f2864b9627
wanglei9927 <leiwang9927@gmail.com>
2020-01-07 10:28:19 +0800

    添加math

---
+++ src/main/java/com/zy/http/HttpTest.java
@@ -0,0 +1,32 @@
+package com.zy.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class HttpTest {
+
+	public static void main(String[] args) {
+		String result = loadJSON("http://localhost:8081/oneplatform/physicalExamination/queryReportList?systemId=SYS-0010&appKey=1111&a=1");
+		
+		System.out.println(result);
+	}
+	
+    public static String loadJSON(String url) {
+        StringBuilder json = new StringBuilder();
+        try {
+            URL oracle = new URL(url);
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
+            String inputLine = null;
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {} catch (IOException e) {}
+        return json.toString();
+    }
+}
--- src/main/java/com/zy/socket/Client.java
+++ src/main/java/com/zy/socket/Client.java
@@ -1,16 +1,18 @@
 package com.zy.socket;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		
 		try {
 			Socket socket = new Socket("127.0.0.1",8080);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+	}	
 	}
+	
 }
